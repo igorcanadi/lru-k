fff432928978e5876f0628e0a1f3f6b44e239b53
Igor Canadi <igor.canadi@gmail.com>
2013-02-19 18:22:20 -0600

    added A0 and zipfian.py

--- zipfian.py
+++ zipfian.py
@@ -1,8 +1,10 @@
 from LRUK import LRUKRP
 import random
+from math import log
 
-N1 = 100
-N2 = 10000
+a = 0.8
+b = 0.2
+N = 1000
 random.seed(20)
 
 class A0:
@@ -20,11 +22,7 @@ class A0:
         if 0 in self._buffer:
             self._buffer[self._buffer.index(0)] = p
         else:
-            for i, b in enumerate(self._buffer):
-                if b > N1:
-                    self._buffer[i] = p
-                    return
-            self._buffer[random.randint(0, len(self._buffer) - 1)] = p
+            self._buffer[self._buffer.index(max(self._buffer))] = p
 
     def clearStats(self):
         self._reqs = 0
@@ -33,30 +31,31 @@ class A0:
     def getHitRatio(self):
         return float(self._hits) / self._reqs
 
-def choose_page_randomly(i):
-    if i % 2 == 0:
-        return random.randint(1, N1)
-    else:
-        return random.randint(N1 + 1, N1 + N2)
+def choose_page_randomly():
+    r = random.random()
+    for i in range(1, N):
+        if r < (float(i) / N) ** (log(a) / log(b)):
+            return i
+    return N
 
 # TODO figure out CRP
 CRP = 10
 
 print "B\tLRU-1\tLRU-2\tLRU-3\tA0"
-for B in (60, 80, 100, 120, 140, 160, 180, 200, 250, 300, 350, 400, 450):
+for B in (40, 60, 80, 100, 120, 140, 160, 180, 200, 300, 500):
     lru = []
     for k in range(1, 4):
         lru.append(LRUKRP(B, k, CRP, None))
     lru.append(A0(B))
 
     # warm-up period
-    for i in range(10 * N1):
-        map(lambda l: l.requestPage(choose_page_randomly(i)), lru)
+    for i in range(1000):
+        map(lambda l: l.requestPage(choose_page_randomly()), lru)
     # clear stats
     map(lambda l: l.clearStats(), lru)
     # real thing
-    for i in range(30 * N1):
-        map(lambda l: l.requestPage(choose_page_randomly(i)), lru)
+    for i in range(3000):
+        map(lambda l: l.requestPage(choose_page_randomly()), lru)
 
     print '%d\t%lf\t%lf\t%lf\t%lf' % (B, lru[0].getHitRatio(), lru[1].getHitRatio(), lru[2].getHitRatio(), lru[3].getHitRatio())
 
